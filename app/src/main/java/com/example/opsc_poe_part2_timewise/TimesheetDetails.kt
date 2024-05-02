1a6991a2bc9c10539f36d5e571b11d31c241d4e4
st10083156 <st10083156@vcconnect.edu.za>
2024-05-02 17:31:47 +0200

    Timesheet Entries Sorted Out

--- app/src/main/java/com/example/opsc_poe_part2_timewise/TimesheetDetails.kt
+++ app/src/main/java/com/example/opsc_poe_part2_timewise/TimesheetDetails.kt
@@ -5,9 +5,13 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.opsc_poe_part2_timewise.databinding.ActivityTimesheetDetailsBinding
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class TimesheetDetails : AppCompatActivity() {
     private lateinit var binding:ActivityTimesheetDetailsBinding
@@ -23,17 +27,22 @@ class TimesheetDetails : AppCompatActivity() {
         }
 
         val receivedName = intent.getStringExtra("name")
-        val receivedDate = intent.getStringExtra("date")
+        val receivedDate = intent.getLongExtra("date",0)
         val receivedCategory = intent.getStringExtra("category")
         val receivedDescription = intent.getStringExtra("description")
-        val receivedTimeSpent = intent.getStringExtra("timeSpent")
+        val receivedTimeSpent = intent.getLongExtra("timeSpent",0)
         val receivedImage = intent.getStringExtra("image")
+        val finalReceivedDate = Date(receivedDate)
+        var timeInHours = receivedTimeSpent?.toLong()?.div(1000)?.div(60)?.div(60)
         binding.nameTV.text = receivedName.toString()
-        binding.dateTV.text = receivedDate.toString()
-        binding.categoryTV.text = receivedCategory.toString()
-        binding.descriptionTV.text = receivedDescription.toString()
-        binding.timeSpentTV.text = receivedTimeSpent.toString()
+        binding.dateTV.text = "Date: ${getDateFromDateObject(finalReceivedDate)}"
+        binding.categoryTV.text = "Category: ${receivedCategory.toString()}"
+        binding.descriptionTV.text = "Description: ${receivedDescription.toString()}"
+        binding.timeSpentTV.text = "Hours worked on: ${timeInHours.toString()}"
         binding.entryImageView.setImageURI(Uri.parse(receivedImage))
+        if(receivedImage==null){
+            binding.entryImageView.setImageDrawable(R.drawable.ic_launcher_foreground.toDrawable())
+        }
 
         binding.returnBtn.setOnClickListener{
             var intent = Intent(this,TimesheetEntriesListDisplay::class.java)
@@ -42,6 +51,14 @@ class TimesheetDetails : AppCompatActivity() {
         }
 
 
-
     }
+
+
+    fun getDateFromDateObject(date: Date): String {
+        val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        val localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+        return localDate.format(dateFormatter)
+    }
+
+
 }
--- app/src/main/java/com/example/opsc_poe_part2_timewise/TimesheetEntriesListDisplay.kt
+++ app/src/main/java/com/example/opsc_poe_part2_timewise/TimesheetEntriesListDisplay.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -35,21 +37,39 @@ class TimesheetEntriesListDisplay : AppCompatActivity() {
         }
         val layoutManager = LinearLayoutManager(this)
         binding.entriesRecyclerView.layoutManager = layoutManager
-        val adapter = MyAdapter(TimesheetEntriesList.getEntriesList(),this)
+        val adapter = MyAdapter(TimesheetEntriesList.getEntriesList(),this, object:MyAdapter.OnItemClickListener{
+            override fun onItemClick(item: TimesheetEntry) {
+                var intent = Intent(applicationContext,TimesheetDetails::class.java)
+                intent.putExtra("name", item.Name)
+                intent.putExtra("category", item.Category.Name)
+                intent.putExtra("description", item.Description)
+                intent.putExtra("date", item.Date.time)
+                intent.putExtra("timeSpent", item.TimeSpent.inWholeMilliseconds)
+                intent.putExtra("image", item.Image.toString())
+                startActivity(intent)
+                finish()
+            }
+        } )
         binding.entriesRecyclerView.adapter=adapter
 
-
+binding.newEntryBtn.setOnClickListener{
+    var intent = Intent(this,NewTimesheetEntry::class.java)
+    startActivity(intent)
+    finish()
+}
     }
 
 
     //ViewHolder class just to hold references to each view in the list_item layout
-    class ItemViewHolder(itemView: View, private val listener: OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
+    class ItemViewHolder(itemView: View,private val listener: MyAdapter.OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
 
         val image: ShapeableImageView = itemView.findViewById(R.id.titleImage)
         val name: TextView = itemView.findViewById(R.id.timesheetNameTV)
         var category: TextView = itemView.findViewById(R.id.categoryTV)
         val description: TextView = itemView.findViewById(R.id.timesheetDescTV)
         val date: TextView = itemView.findViewById(R.id.dateTV)
+        val button : Button = itemView.findViewById(R.id.itemDisplayBtn)
+
 
         fun bind(item: TimesheetEntry) {
             // Bind item data to views here
@@ -57,37 +77,35 @@ class TimesheetEntriesListDisplay : AppCompatActivity() {
             category.text = item.Category.Name
             description.text = item.Description
             date.text = item.Date.toString()
-
-            // Set up click listener
-            itemView.setOnClickListener {
+            button.setOnClickListener {
                 listener.onItemClick(item)
             }
+
         }
     }
 
-
+    /*var intent = Intent(parent.context,TimesheetDetails::class.java)
+    intent.putExtra("name", item.Name)
+    intent.putExtra("category", item.Category.Name)
+    intent.putExtra("description", item.Description)
+    intent.putExtra("date", item.Date)
+    intent.putExtra("timeSpent", item.TimeSpent.inWholeMilliseconds)
+    intent.putExtra("image", item.Image.toString())
+    activity.startActivity(intent)
+    activity.finish()*/
     //Adapter class to work with the viewss and assign values based on user data
-    class MyAdapter(private val data: List<TimesheetEntry>, private val activity: Activity) :
+    class MyAdapter(private val data: List<TimesheetEntry>,
+                    private val activity: Activity,
+                    private val listener: OnItemClickListener) :
         RecyclerView.Adapter<ItemViewHolder>() {
+        interface OnItemClickListener {
+            fun onItemClick(item: TimesheetEntry)
+        }
         //creates viewholder objects to hold the views for manipulation
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
             val view =
                 LayoutInflater.from(parent.context).inflate(R.layout.timesheet_list_item, parent, false)
-            return ItemViewHolder(view, object:OnItemClickListener
-            {
-                override fun onItemClick(item: TimesheetEntry) {
-
-                   var intent = Intent(parent.context,TimesheetDetails::class.java)
-                    intent.putExtra("name", item.Name)
-                    intent.putExtra("category", item.Category.Name)
-                    intent.putExtra("description", item.Description)
-                    intent.putExtra("date", item.Date)
-                    intent.putExtra("timeSpent", item.TimeSpent.inWholeMilliseconds)
-                    intent.putExtra("image", item.Image.toString())
-                    activity.startActivity(intent)
-                    activity.finish()
-                }
-            })
+            return ItemViewHolder(view, listener)
         }
 
         //used to bind values to the viewholder's properties
@@ -98,6 +116,7 @@ class TimesheetEntriesListDisplay : AppCompatActivity() {
             holder.category.text = currentItem.Category.Name
             holder.description.text = currentItem.Description
             holder.date.text = currentItem.Date.toString() // Set the date directly from the TimesheetEntry
+            holder.bind(currentItem)
         }
 
 
@@ -112,8 +131,8 @@ class TimesheetEntriesListDisplay : AppCompatActivity() {
             return dateFormat.parse(inputText) ?: Date()
         }
     }
-    interface OnItemClickListener {
-        fun onItemClick(item: TimesheetEntry)
-    }
+
+
+
 
 }
